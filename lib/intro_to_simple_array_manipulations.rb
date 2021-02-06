53058c077664b905dbe9f565ab174a58eed28978
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-06 01:04:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,19 +3,19 @@ def using_concat(array, array2)
 end
 
 def using_insert(array, element)
-  
+  array.insert(element)
 end
 
 def using_uniq(array)
-  
+  array.uniq
 end
 
 def using_flatten(array)
-  
+  array.flatten
 end
 
 def using_delete(array, string)
-  
+  array.delete(string)
 end
 
 def using_delete_at(array, integer)
